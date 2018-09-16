f08d3b1a679facb4030f7d5b36549bf345db694e
mrZalli <misterzalli@gmail.com>
2018-09-16 07:51:04 +0300

    Add blending, HSV and other methods to `Alpha`

--- src/color/alpha.rs
+++ src/color/alpha.rs
@@ -1,8 +1,12 @@
+use cgmath::Deg;
+
 use super::*;
 
 /// A transparent color `C` with an alpha channel of type `A`.
 ///
 /// Alpha of 1 means the color is fully opaque, and alpha of 0 means it's fully transparent.
+///
+/// This uses a straight alpha, not a premultiplied alpha.
 #[derive(Debug, Default, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct Alpha<C, A> {
     pub color: C,
@@ -13,12 +17,57 @@ impl<C, A> Alpha<C, A> {
     pub fn new(color: C, alpha: A) -> Self {
         Alpha { color, alpha }
     }
+
+    pub fn to_tuple2(self) -> (C, A) {
+        (self.color, self.alpha)
+    }
+}
+
+impl<T, S, A> Alpha<RGBColor<T, S>, A> {
+    pub fn to_tuple4(self) -> (T, T, T, A) {
+        (self.color.r, self.color.g, self.color.b, self.alpha)
+    }
+}
+
+impl<T, S> Alpha<RGBColor<T, S>, T> {
+    pub fn to_array4(self) -> [T; 4] {
+        [self.color.r, self.color.g, self.color.b, self.alpha]
+    }
+}
+
+impl<S, A> Alpha<HSVColor<S>, A> {
+    pub fn to_tuple4(self) -> (Deg<f32>, f32, f32, A) {
+        (self.color.h, self.color.s, self.color.v, self.alpha)
+    }
+}
+
+impl<S> Alpha<HSVColor<S>, f32> {
+    pub fn to_array4(self) -> [f32; 4] {
+        [self.color.h.0, self.color.s, self.color.v, self.alpha]
+    }
 }
 
 impl Alpha<LinRGBColor, f32> {
-    /// Blends this color with the background color using this color's alpha.
-    pub fn blend(self, other: LinRGBColor) -> LinRGBColor {
-        self.color.blend(other, self.alpha)
+    /// Alpha blend this color with the given opaque background color.
+    pub fn blend(self, bg: LinRGBColor) -> LinRGBColor {
+        self.color.blend(bg, self.alpha)
+    }
+
+    /// Alpha blend this color with the given background color.
+    pub fn alpha_blend<T: Into<Self>>(self, bg: T) -> Self {
+        let bg = bg.into();
+
+        let alpha = self.alpha + bg.alpha * (1.0 - self.alpha);
+        if alpha == 0.0 {
+            return Alpha::default();
+        }
+
+        let color = (
+            self.color * self.alpha +
+            bg.color * bg.alpha * (1.0 - self.alpha)
+        ) / alpha;
+
+        Alpha { color, alpha }
     }
 }
 
@@ -51,3 +100,10 @@ impl<S> From<RGBColor<f32, S>> for Alpha<RGBColor<f32, S>, f32> {
         Alpha::new(color, 1.0)
     }
 }
+
+/*macro_rules! alpha_impl_ops {
+    ( $( $op_trait: ident ),+ ; $( $op_fun:ident ),+ ) => { $(
+        impl<C, A> $op_trait for Alpha<C, A>
+            where C: $op_trait
+    ),+ };
+}*/
